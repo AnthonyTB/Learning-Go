ab9ec391f0f01cd093e049755b7ee7720595cb93
AnthonyTB <anthonylol@protonmail.com>
2020-10-11 12:27:44 -0700

    Completed first exercise: Learning how to use printing

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
+func mainPre() {
 	fmt.Println("Hello Front End Masters!")
 }
---
+++ practice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func mainPrac() {
+	// creates new line
+	fmt.Println("Hello World")
+	// no new line
+	fmt.Print("Hello Again")
+	// takes a argument
+	fmt.Printf("Hello Again %s. it is October %d.", "Anthony", 11)
+}
